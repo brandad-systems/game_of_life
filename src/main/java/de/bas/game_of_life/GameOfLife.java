88ca33341275f5298f9761134deca844f2eece5a
Shakti Saxena (DATEV) <Shakti.Saxena@brandad-systems.de>
2022-01-26 13:36:59 +0100

    initial stand

---
+++ src/main/java/de/bas/game_of_life/GameOfLife.java
@@ -0,0 +1,21 @@
+package de.bas.game_of_life;
+
+public class GameOfLife {
+
+    char [][] board;
+
+    public GameOfLife(char[][] board) {
+        this.board = board;
+    }
+
+    int calculateAliveNeighbours(int x, int y){
+        // return no. of alive neighbours of char in position x,y
+    return 0;
+    }
+
+    boolean isAlive(int x, int y)
+    {
+        return true;
+    }
+
+}
---
+++ src/test/java/de/bas/game_of_life/GameOfLifeTest.java
@@ -0,0 +1,14 @@
+package de.bas.game_of_life;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameOfLifeTest {
+
+    @Test
+    void calculateAliveNeighbours() {
+    
+
+    }
+}
