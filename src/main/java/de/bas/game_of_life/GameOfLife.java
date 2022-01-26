d36fba08e1aae5b09470533d4d24ce4a7b85f9d6
fabian.reichenberg <fabian.reichenberg@outlook.de>
2022-01-26 14:32:13 +0100

    changes

--- src/main/java/de/bas/game_of_life/GameOfLife.java
+++ src/main/java/de/bas/game_of_life/GameOfLife.java
@@ -1,7 +1,8 @@
 package de.bas.game_of_life;
 
-public class GameOfLife {
+import static de.bas.game_of_life.MatrixPaneConverter.LIVING_CELL;
 
+public class GameOfLife {
     char [][] board;
 
     public GameOfLife(char[][] board) {
@@ -13,9 +14,11 @@ public class GameOfLife {
     return 0;
     }
 
-    boolean isAlive(int x, int y)
-    {
-        return true;
+    boolean isAlive(int x, int y) {
+        if(board == null || x < 0 || y < 0 || y >= board.length || x >= board[0].length) {
+            return false;
+        }
+        return board[y][x] == LIVING_CELL;
     }
 
 }
--- src/test/java/de/bas/game_of_life/GameOfLifeTest.java
+++ src/test/java/de/bas/game_of_life/GameOfLifeTest.java
@@ -2,13 +2,34 @@ package de.bas.game_of_life;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameOfLifeTest {
 
+    static char LIVING_CELL = '■';
+    static char DEAD_CELL = ' ';
     @Test
     void calculateAliveNeighbours() {
-    
 
+    }
+
+
+    @Test
+    void isAlive_when_Board_IS_NULL() {
+        var gameOfLife = new GameOfLife(null);
+        assertThat(gameOfLife.isAlive(0,0)).isFalse();
+    }
+
+    @Test
+    void isAlive() {
+        char [][] board= new char[][]{
+                {DEAD_CELL,DEAD_CELL},  // x=0;y=0;    x=1;y=0
+                {LIVING_CELL,DEAD_CELL} // x=0;y=1;    x=1;y=1
+        };
+        var gameOfLife = new GameOfLife(board);
+        assertThat(gameOfLife.isAlive(0,1)).isTrue();
+        assertThat(gameOfLife.isAlive(-1,-1)).isFalse();
+        assertThat(gameOfLife.isAlive(2,2)).isFalse();
     }
 }
