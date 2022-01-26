9c9b0526900bb86d630d2666e4b34aa514fdda2f
Tanja Maier (DATEV) <tanja.maier@brandad-systems.de>
2022-01-26 15:59:07 +0100

    calculate alive neighbours, calculate next generation cell state with tests

--- src/main/java/de/bas/game_of_life/GameOfLife.java
+++ src/main/java/de/bas/game_of_life/GameOfLife.java
@@ -1,24 +1,44 @@
 package de.bas.game_of_life;
 
+import static de.bas.game_of_life.MatrixPaneConverter.DEAD_CELL;
 import static de.bas.game_of_life.MatrixPaneConverter.LIVING_CELL;
 
 public class GameOfLife {
-    char [][] board;
+    char[][] board;
 
     public GameOfLife(char[][] board) {
         this.board = board;
     }
 
-    int calculateAliveNeighbours(int x, int y){
-        // return no. of alive neighbours of char in position x,y
-    return 0;
+    // return no. of alive neighbours of char in position x,y
+    int calculateAliveNeighbours(int x, int y) {
+        int count = 0;
+        for (int row = x - 1; row <= x + 1; row++) {
+            for (int column = y - 1; column <= y + 1; column++) {
+                if (!(row == x && column == y) && isAlive(row, column))
+                    count++;
+            }
+        }
+        return count;
     }
 
     boolean isAlive(int x, int y) {
-        if(board == null || x < 0 || y < 0 || y >= board.length || x >= board[0].length) {
+        if (board == null || x < 0 || y < 0 || y >= board.length || x >= board[0].length) {
             return false;
         }
-        return board[y][x] == LIVING_CELL;
+        return returnState(x,y) == LIVING_CELL;
+    }
+
+    char calculateNextGenerationCellState(int x, int y) {
+
+        int count = calculateAliveNeighbours(x,y);
+        if(count < 2 || count> 3) return DEAD_CELL;
+        if(count == 3) return LIVING_CELL;
+        return returnState(x,y);
+    }
+
+    private char returnState(int x, int y){
+        return board[y][x];
     }
 
 }
--- src/test/java/de/bas/game_of_life/GameOfLifeTest.java
+++ src/test/java/de/bas/game_of_life/GameOfLifeTest.java
@@ -9,27 +9,132 @@ class GameOfLifeTest {
 
     static char LIVING_CELL = '■';
     static char DEAD_CELL = ' ';
-    @Test
-    void calculateAliveNeighbours() {
 
+    @Test
+    void calculateNextGenerationCellState_should_return_alive_cell_with_3_alive_neighbours() {
+        char[][] board = new char[][]{
+                {DEAD_CELL, LIVING_CELL},  // x=0;y=0;    x=1;y=0
+                {LIVING_CELL, LIVING_CELL} // x=0;y=1;    x=1;y=1
+        };
+        var gameOfLife = new GameOfLife(board);
+        assertThat(gameOfLife.calculateNextGenerationCellState(0,0)).isEqualTo(LIVING_CELL);
     }
 
+    @Test
+    void calculateNextGenerationCellState_should_stay_dead_cell_with_2_alive_neighbours() {
+        char[][] board = new char[][]{
+                {DEAD_CELL, DEAD_CELL},  // x=0;y=0;    x=1;y=0
+                {LIVING_CELL, LIVING_CELL} // x=0;y=1;    x=1;y=1
+        };
+        var gameOfLife = new GameOfLife(board);
+        assertThat(gameOfLife.calculateNextGenerationCellState(0,0)).isEqualTo(DEAD_CELL);
+    }
+
+    @Test
+    void calculateNextGenerationCellState_should_stay_live_cell_with_2_alive_neighbours() {
+        char[][] board = new char[][]{
+                {LIVING_CELL, DEAD_CELL},  // x=0;y=0;    x=1;y=0
+                {LIVING_CELL, LIVING_CELL} // x=0;y=1;    x=1;y=1
+        };
+        var gameOfLife = new GameOfLife(board);
+        assertThat(gameOfLife.calculateNextGenerationCellState(0,0)).isEqualTo(LIVING_CELL);
+    }
+
+   /*
+    1. Any live cell with fewer than two live neighbours dies, ✔
+    as if caused by underpopulation.
+    2. Any live cell with more than three live neighbours dies, ✔
+    as if by overcrowding.
+    3. Any live cell with two or three live neighbours lives
+    on to the next generation.
+    4. Any dead cell with exactly three live neighbours becomes
+    a live cell.*/
+   @Test
+   void calculateNextGenerationCellState_should_return_dead_cell_with_more_than_3_alive_neighbours() {
+       char[][] board = new char[][]{
+               {LIVING_CELL, LIVING_CELL, LIVING_CELL},  // x=0;y=0;    x=1;y=0   x=2;y=0
+               {LIVING_CELL, LIVING_CELL, DEAD_CELL} // x=0;y=1;        x=1;y=1   x=2;y=1
+       };
+       var gameOfLife = new GameOfLife(board);
+       assertThat(gameOfLife.calculateNextGenerationCellState(1,1)).isEqualTo(DEAD_CELL);
+   }
+
+    @Test
+    void calculateNextGenerationCellState_should_return_dead_cell_with_less_than_2_alive_neighbours() {
+        char[][] board = new char[][]{
+                {LIVING_CELL, DEAD_CELL},  // x=0;y=0;    x=1;y=0
+                {LIVING_CELL, DEAD_CELL} // x=0;y=1;        x=1;y=1
+        };
+        var gameOfLife = new GameOfLife(board);
+        assertThat(gameOfLife.calculateNextGenerationCellState(0,0)).isEqualTo(DEAD_CELL);
+    }
+
+    @Test
+    void calculateNextGenerationCellState_should_return_dead_cell_with_zero_alive_neighbours() {
+        char[][] board = new char[][]{
+                {LIVING_CELL, DEAD_CELL},  // x=0;y=0;    x=1;y=0
+                {DEAD_CELL, DEAD_CELL} // x=0;y=1;        x=1;y=1
+        };
+        var gameOfLife = new GameOfLife(board);
+        assertThat(gameOfLife.calculateNextGenerationCellState(0,0)).isEqualTo(DEAD_CELL);
+    }
+
+    @Test
+    void calculateAliveNeighbours() {
+        char[][] board = new char[][]{
+                {DEAD_CELL, DEAD_CELL},  // x=0;y=0;    x=1;y=0
+                {LIVING_CELL, DEAD_CELL} // x=0;y=1;    x=1;y=1
+        };
+        var gameOfLife = new GameOfLife(board);
+        assertThat(gameOfLife.calculateAliveNeighbours(0,0)).isEqualTo(1);
+    }
+
+    @Test
+    void calculateAliveNeighbours_out_of_bounds_living_cell_in_range() {
+        char[][] board = new char[][]{
+                {LIVING_CELL, DEAD_CELL},  // x=0;y=0;    x=1;y=0
+                {LIVING_CELL, DEAD_CELL} // x=0;y=1;    x=1;y=1
+        };
+        var gameOfLife = new GameOfLife(board);
+        assertThat(gameOfLife.calculateAliveNeighbours(-1,-1)).isEqualTo(1);
+    }
+
+    @Test
+    void calculateAliveNeighbours_out_of_bounds_dead_cell_in_range() {
+        char[][] board = new char[][]{
+                {DEAD_CELL, DEAD_CELL},  // x=0;y=0;    x=1;y=0
+                {LIVING_CELL, DEAD_CELL} // x=0;y=1;    x=1;y=1
+        };
+        var gameOfLife = new GameOfLife(board);
+        assertThat(gameOfLife.calculateAliveNeighbours(-1,-1)).isEqualTo(0);
+    }
 
     @Test
     void isAlive_when_Board_IS_NULL() {
         var gameOfLife = new GameOfLife(null);
-        assertThat(gameOfLife.isAlive(0,0)).isFalse();
+        assertThat(gameOfLife.isAlive(0, 0)).isFalse();
     }
 
     @Test
     void isAlive() {
-        char [][] board= new char[][]{
-                {DEAD_CELL,DEAD_CELL},  // x=0;y=0;    x=1;y=0
-                {LIVING_CELL,DEAD_CELL} // x=0;y=1;    x=1;y=1
+        char[][] board = new char[][]{
+                {DEAD_CELL, DEAD_CELL},  // x=0;y=0;    x=1;y=0
+                {LIVING_CELL, DEAD_CELL} // x=0;y=1;    x=1;y=1
         };
         var gameOfLife = new GameOfLife(board);
-        assertThat(gameOfLife.isAlive(0,1)).isTrue();
-        assertThat(gameOfLife.isAlive(-1,-1)).isFalse();
-        assertThat(gameOfLife.isAlive(2,2)).isFalse();
+        assertThat(gameOfLife.isAlive(0, 1)).isTrue();
+        assertThat(gameOfLife.isAlive(-1, -1)).isFalse();
+        assertThat(gameOfLife.isAlive(2, 2)).isFalse();
     }
+
+    @Test
+    void isAlive_invalidBoard() {
+        char[][] board = new char[][]{
+                {'A', 'B'},  // x=0;y=0;    x=1;y=0
+                {'C', 'D'} // x=0;y=1;    x=1;y=1
+        };
+        var gameOfLife = new GameOfLife(board);
+        assertThat(gameOfLife.isAlive(0, 0)).isFalse();
+    }
+
 }
